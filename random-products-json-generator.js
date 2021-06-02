bb53c9cdad6dbf2e054ea8cef47a84ae4276d5a3
md moin <45753713+mdmoin7@users.noreply.github.com>
2021-06-02 09:39:48 +0530

    Update random-products-json-generator.js

--- random-products-json-generator.js
+++ random-products-json-generator.js
@@ -1,27 +1,38 @@
-const generateProducts = (arrayLength) => (minCategory, maxCategory) => (minCost, maxCost) => {
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  colors,
+  names
+} from 'unique-names-generator';
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-    let a = new Array(arrayLength);
-
-    for (let i = 0; i < a.length; i++) {
-        const minCat = minCategory;
-        const maxCat = maxCategory;
-        const minPrice = minCost;
-        const maxPrice = maxCost;
-        const randomCategory = Math.floor(Math.random() * (+maxCat - +minCat)) + +minCat;
-        const randomStock = Math.floor((Math.random() * 1000) % 2);
-        const randomPrice = (Math.random() * (+maxPrice - +minPrice)) + +minPrice;
-        a[i] = {
-            "productId": 1000 + i + 1,
-            "productCategory": `Category ${randomCategory}`,
-            "productName": `Product ${i + 1}`,
-            "productImage": `https://picsum.photos/400?image=${Math.floor(Math.random() * 1000)}`,
-            "productStock": !!randomStock,
-            "productPrice": randomPrice,
-            "salePrice":!!Math.floor((Math.random() * 1000) % 2)?randomPrice*((Math.floor(Math.random() * (50 - 10) + 10))/100):randomPrice
-        }
-    }
-    console.log('Below is your products json array');
-    console.log(JSON.stringify(a));
-}
-// sample execution
-generateProducts(100)(1, 5)(10000, 100000);
+const createDump = (arrayLength = 100) => {
+  let a = new Array(arrayLength);
+  for (let i = 0; i < a.length; i++) {
+    const minPrice = 1000;
+    const maxPrice = 50000;
+    const randomStock = Math.floor(Math.random() * 100);
+    const randomPrice = Math.random() * (+maxPrice - +minPrice) + +minPrice;
+    const salePrice = !!Math.floor((Math.random() * 1000) % 2)
+      ? randomPrice * (Math.floor(Math.random() * (50 - 10) + 10) / 100)
+      : randomPrice;
+    const randomRating = Math.random() * (5 - 1) + 1;
+    a[i] = {
+      productName: uniqueNamesGenerator({
+        dictionaries: [adjectives, colors, names],
+        separator: ' '
+      }),
+      productImage: `https://picsum.photos/400?image=${Math.floor(
+        Math.random() * 1000
+      )}`,
+      productStock: randomStock,
+      productPrice: randomPrice.toFixed(2),
+      productSalePrice: salePrice.toFixed(2),
+      rating: parseInt(randomRating.toFixed(0))
+    };
+  }
+  return a;
+};
+console.log(JSON.stringify(createDump()));
